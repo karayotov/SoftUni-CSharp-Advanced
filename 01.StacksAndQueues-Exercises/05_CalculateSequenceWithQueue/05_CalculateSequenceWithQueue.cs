df5f3f10f5860ab39d95ad1070a34ef1d5127db9
karayotov <32219166+karayotov@users.noreply.github.com>
2018-01-25 16:25:38 +0200

    Multidimensional Arrays - Lab
    _02_SquareWithMaxSum

--- 01.StacksAndQueues-Exercises/05_CalculateSequenceWithQueue/05_CalculateSequenceWithQueue.cs
+++ 01.StacksAndQueues-Exercises/05_CalculateSequenceWithQueue/05_CalculateSequenceWithQueue.cs
@@ -1,8 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+
 class Program
 {
     static void Main()
     {
+        long n = long.Parse(Console.ReadLine());
+        Queue<long> queue = new Queue<long>();
+        
 
+        long s1 = n, s2, s3, s4;
+
+        queue.Enqueue(s1);
+
+        int loop = 50;
+
+        while (queue.Count <= loop)
+        {
+            var s = queue.Dequeue();
+
+            Console.Write(s + " ");
+
+            s2 = s + 1;
+            s3 = 2 * s + 1;
+            s4 = s + 2;
+            
+            queue.Enqueue(s2);
+            queue.Enqueue(s3);
+            queue.Enqueue(s4);
+
+            loop --;
+
+        }
+        while (loop-- > 0)
+        {
+            Console.Write(queue.Dequeue() + " ");
+        }
     }
 }
---
+++ 02_MultidimensionalArray-Lab/02_SquareWithMaxSum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_SquareWithMaxSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndCols = Console.ReadLine().Split(new string[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int[,] matrix = new int[rowsAndCols[0], rowsAndCols[1]];
+
+            int sum = 0;
+
+            int rowIndex = 0, colIndex = 0;
+
+            for (int rows = 0; rows < rowsAndCols[0]; rows++)
+            {
+                var values = Console.ReadLine().Split(new string[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+                for (int col = 0; col < rowsAndCols[1]; col++)
+                {
+                    matrix[rows, col] = values[col];
+                }
+            }
+
+            for (int rows = 0; rows < matrix.GetLength(0) - 1; rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1) - 1; cols++)
+                {
+                    var tempSum = matrix[rows, cols] + matrix[rows, cols + 1] + matrix[rows + 1, cols] +
+                                  matrix[rows + 1, cols + 1];
+                    if (tempSum > sum)
+                    {
+                        sum = tempSum;
+                        rowIndex = rows;
+                        colIndex = cols;
+                    }
+                }
+            }
+            Console.WriteLine(matrix[rowIndex, colIndex] + " " + matrix[rowIndex, colIndex + 1] + " ");
+            Console.WriteLine(matrix[rowIndex + 1, colIndex] + " " + matrix[rowIndex + 1, colIndex + 1] + " ");
+        }
+    }
+}
