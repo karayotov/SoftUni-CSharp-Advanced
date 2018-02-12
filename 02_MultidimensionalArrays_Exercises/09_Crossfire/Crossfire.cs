c54ea16c30b54fedfca5941da4aba7a09d13fc88
karayotov <32219166+karayotov@users.noreply.github.com>
2018-02-12 10:41:33 +0200

    C# Advanced The Exam and after
    01 - 100/100 - 1h30min
    02 - 30/100 - 4h30min
    after exam
    03 - 100/100 - 1h30min

---
+++ 02_MultidimensionalArrays_Exercises/09_Crossfire/Crossfire.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+
+namespace _09_Crossfire
+{
+    class Crossfire
+    {
+        static void Main(string[] args)
+        {
+            long[] matrixData = Console.ReadLine()
+                .Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToArray();
+            long rowCount = matrixData[0];
+            long colCount = matrixData[1];
+            string[][] stringLand = new string[rowCount][];
+
+            long index = 0;
+
+            for (long rowIndex = 0; rowIndex < rowCount; rowIndex++)
+            {
+                stringLand[rowIndex] = new string[colCount];
+
+                for (long colIndex = 0; colIndex < colCount; colIndex++)
+                {
+                    ++index;
+                    string stringElement = index.ToString();
+
+                    stringLand[rowIndex][colIndex] = stringElement;
+                }
+            }
+            string input;
+            while ((input = Console.ReadLine()) != "Nuke it from orbit")
+            {
+                long[] commands = input
+                    .Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(long.Parse)
+                    .ToArray();
+
+                long rowHit = long.Parse(commands[0].ToString());
+                long colHit = long.Parse(commands[1].ToString());
+                int rangeHit = int.Parse(commands[2].ToString());
+
+                NukeThemAll(rowHit, colHit, rangeHit, rowCount, colCount, stringLand);
+
+                stringLand = TransferWhatIsLeft(stringLand);
+            }
+            PrintWhatIsLeft(stringLand);
+
+        }
+
+        private static void PrintWhatIsLeft(string[][] stringLand)
+        {
+            for (int row = 0; row < stringLand.Length; row++)
+            {
+                for (int col = 0; col < stringLand[row].Length; col++)
+                {
+                        Console.Write(stringLand[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static string[][] TransferWhatIsLeft(string[][] stringLand)
+        {
+            string[][] survivals = new string[stringLand.Length][];
+            long newRow = 0;
+            long newCol = 0;
+
+            for (int rowIndex = 0; rowIndex < stringLand.Length; rowIndex++)
+            {
+                survivals[newRow] = new string[stringLand[rowIndex].Length];
+
+                for (int colIndex = 0; colIndex < stringLand[rowIndex].Length; colIndex++)
+                {
+                    if (stringLand[rowIndex][colIndex] != String.Empty)
+                    {
+                        survivals[newRow][newCol] = stringLand[rowIndex][colIndex];
+                        newCol++;
+                    }
+                }
+                newRow++;
+                newCol = 0;
+            }
+            stringLand = survivals;
+            return stringLand;
+        }
+
+        public static void NukeThemAll(long rowHit, long colHit, int hitRange, long rowCount, long colCount,
+            string[][] stringLand)
+        {
+
+            if (InLand(rowHit, colHit, rowCount, colCount))
+            {
+                stringLand[rowHit][colHit] = String.Empty;
+            }
+            for (int i = 1; i <= hitRange; i++)
+            {
+                colHit -= i;
+                if (InLand(rowHit, colHit, rowCount, colCount))
+                    stringLand[rowHit][colHit] = String.Empty;  //leftBlast
+                colHit += i;
+                colHit += i;
+                if (InLand(rowHit, colHit, rowCount, colCount))
+                    stringLand[rowHit][colHit] = String.Empty;  //rightBlast
+                colHit -= i;
+                rowHit -= i;
+                if (InLand(rowHit, colHit, rowCount, colCount))
+                    stringLand[rowHit][colHit] = String.Empty;  //upBlast
+                rowHit += i;
+                rowHit += i;
+                if (InLand(rowHit, colHit, rowCount, colCount))
+                    stringLand[rowHit][colHit] = String.Empty;  //downBlast
+                rowHit -= i;
+            }
+        }
+
+        private static bool InLand(long rowHit, long colHit, long rowCount, long colCount)
+        {
+            return rowHit < rowCount && colHit < colCount && rowHit >= 0 && colHit >= 0;
+        }
+    }
+}
--- Exam Prepare/02_AdvancedExamRetake-03Sep2017/02_AdvancedExamRetake-03Sep2017/DangerousFloor.cs
+++ Exam Prepare/02_AdvancedExamRetake-03Sep2017/02_AdvancedExamRetake-03Sep2017/DangerousFloor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Net.Sockets;
 
 namespace _02_AdvancedExamRetake_03Sep2017
 {
@@ -7,56 +8,136 @@ namespace _02_AdvancedExamRetake_03Sep2017
     {
         static void Main(string[] args)
         {
-            string inputLine;
 
-            string[][] floorMatrix = new string[8][];
+            char[][] floorMatrix = new char[8][];
 
-            for (int row = 0; row < floorMatrix.GetLength(0); row++)
+            for (int row = 0; row < floorMatrix.Length; row++)
             {
-                floorMatrix[row] = Console.ReadLine().Split(new string[]{","}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                floorMatrix[row] = Console.ReadLine()
+                    .Split(',')
+                    .Select(char.Parse)
+                    .ToArray();
             }
-
+            Console.WriteLine();
+            string inputLine;
             while ((inputLine = Console.ReadLine()) != "END")
             {
-                
-                string gender = inputLine[0].ToString();
-                int startingRow = int.Parse(inputLine[1].ToString());
+                char emptyPlace = 'x';
+                char gender = inputLine[0];
+                int startRow = int.Parse(inputLine[1].ToString());
                 int startCol = int.Parse(inputLine[2].ToString());
                 int finalRow = int.Parse(inputLine[4].ToString());
                 int finalCol = int.Parse(inputLine[5].ToString());
 
 
+
+                if (EverithingIsTochno(floorMatrix, gender, startRow, startCol, finalRow, finalCol))
+                {
+                    floorMatrix[startRow][startCol] = emptyPlace;
+                    floorMatrix[finalRow][finalCol] = gender;
+                }
+
             }
-
-
         }
 
+        public static bool EverithingIsTochno(char[][] floorMatrix, char gender, int startRow, int startCol, int finalRow, int finalCol)
+        {
+            if (floorMatrix[startRow][startCol] != gender)
+            {
+                Console.WriteLine("There is no such a piece!");
+                return false;
+            }
+
+            bool peaceMoveIsOk = false;
+
+            switch (gender)
+            {
+                
+                case 'K':
+                    if (KingValidMove(startRow, startCol, finalRow, finalCol)) peaceMoveIsOk = true;
+                    break;
+                case 'R':
+                    if (RookValidMove(startRow, startCol, finalRow, finalCol)) peaceMoveIsOk = true;
+                    break;
+                case 'B':
+                    if (BishopValidMove(startRow, startCol, finalRow, finalCol)) peaceMoveIsOk = true;
+                    break;
+                case 'Q':
+                    if (QueenValidMove(startRow, startCol, finalRow, finalCol)) peaceMoveIsOk = true;
+                    break;
+                case 'P':
+                    if (PawnValidMove(startRow, startCol, finalRow, finalCol)) peaceMoveIsOk = true;
+                    break;
+                default:
+                    throw new Exception();
+            }
+
+            if (peaceMoveIsOk == false)   //|| NotMovingPeace(startRow, startCol, finalCol, finalRow)
+            {
+                Console.WriteLine("Invalid move!");
+                return false;
+            }
+
+            if (MoveOutOfBoard(finalRow, finalCol))
+            {
+                Console.WriteLine("Move go out of board!");
+                return false;
+            }
+            return true;
+
+        }
         public static bool RookValidMove(int startRow, int startCol, int finalRow, int finalCol)
         {
-            if ((startRow == finalRow && startCol != finalCol)   ||   (startRow != finalRow && startCol == finalCol))
-            {
-                return true;
-            }
-            return false;
+            return startRow == finalRow || startCol == finalCol;
         }
 
         public static bool BishopValidMove(int startRow, int startCol, int finalRow, int finalCol)
         {
-            if (Math.Abs(startRow - finalRow) == Math.Abs(startCol - finalCol))
-            {
-                return true;
-            }
-            return false;
+            return Math.Abs(startRow - finalRow) == Math.Abs(startCol - finalCol);
         }
 
         public static bool QueenValidMove(int startRow, int startCol, int finalRow, int finalCol)
         {
-            if (BishopValidMove(startRow, startCol, finalRow, finalCol) && RookValidMove(startRow, startCol, finalRow, finalCol))
+            return BishopValidMove(startRow, startCol, finalRow, finalCol) || RookValidMove(startRow, startCol, finalRow, finalCol);
+        }
+
+        public static bool KingValidMove(int startRow, int startCol, int finalRow, int finalCol)
+        {
+            int rowRange = Math.Abs(startRow - finalRow);
+            int colRange =  Math.Abs(startCol - finalCol);
+
+            return rowRange == 1 && colRange == 1 || rowRange == 0 && colRange == 1 || rowRange == 1 && colRange == 0;
+        }
+
+        public static bool PawnValidMove(int startRow, int startCol, int finalRow, int finalCol)
+        {
+            bool pawnIsOnTheLastPosition = startRow == 0;
+
+            if (finalRow == startRow - 1 && startCol == finalCol && pawnIsOnTheLastPosition == false)
             {
                 return true;
             }
             return false;
         }
 
+        public static bool MoveOutOfBoard(int finalRow, int finalCol)
+        {
+            if (finalRow >= 8 || finalRow < 0 || finalCol >= 8 || finalCol < 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool InvalidStart(int startRow, int startCol)
+        {
+            return startRow >= 8 || startRow < 0 || startCol >= 8 || startCol < 0;
+        }
+
+        public static bool NotMovingPeace(int startRow, int startCol, int finalRow, int finalCol)
+        {
+            return startRow == finalRow && startCol == finalCol;
+        }
     }
 }
